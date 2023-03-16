5455b1f71da52774a3a815af1d149ada7a5a461e
filonenkoiv <68235606+filonenkoiv@users.noreply.github.com>
2023-03-16 21:25:10 +0200

    Create first_test.py

---
+++ first_test.py
@@ -0,0 +1,35 @@
+def spam(number):
+    number = int(input("Number: "))
+    return 'bulochka' * number
+
+
+def my_sum(list_of_numbers):
+
+    pass
+    list_one = [10, 2, 0]
+    list_two = [10, 20, 100, 14]
+    list_three = [-50, 200, -100, -50, 12]
+
+    return list_one[0] + list_one[1] + list_one[2]
+    return list_two[0] + list_two[1] + list_two[2] + list_two[3]
+    return list_three[0] + list_three[1] + list_three[2] + list_three[3] + list_three[4]
+
+def shortener(string):
+    """
+     Функция получает на вход длинную строку с множеством слов.
+     Она должна вернуть ту же строку, но в словах, которые длиннее 6 символов,
+     функция должна вместо всех символов после шестого поставить одну звездочку.
+     Пример: Из слова 'verwijdering' должно получиться 'verwij*'
+    """
+    pass
+
+
+
+def compare_ends(words):
+    """
+    Функция получает на вход массив строк. Вернуть надо количество строк,
+    которые не короче двух символов и у которых первый и последний
+    символ совпадают.
+    """
+    pass
+ 
